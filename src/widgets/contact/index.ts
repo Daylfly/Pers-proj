d34748147dc309637f944cfd6f6e36af1b5fa8f9
Daylfly <dazzlenolik@gmail.com>
2025-12-09 14:16:42 +0300

    succes

--- src/widgets/contact/index.ts
+++ src/widgets/contact/index.ts
@@ -1,6 +1,3 @@
 // src/widgets/contact/index.ts
 
-export { default } from "./ui/section.ui";
-export const ContactSection = (await import("./ui/section.ui")).default;
-// или просто:
-export { default as ContactSection } from "./ui/section.ui"; // это тоже работает в новых версиях
+export { default as ContactSection } from "./ui/section.ui";
